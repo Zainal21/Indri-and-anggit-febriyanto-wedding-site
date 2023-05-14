31fc13c1fcdef845af5dbe4a7495e400e8863744
Zainal21 <Zainalarifin080718@gmail.com>
2023-05-14 20:02:40 +0700

    feat : music control

--- src/components/SoundTrackPlayer.jsx
+++ src/components/SoundTrackPlayer.jsx
@@ -1,18 +1,16 @@
 import ReactAudioPlayer from 'react-audio-player'
 import React from 'react'
 import AudioPlayer from 'react-h5-audio-player'
-
 import 'react-h5-audio-player/lib/styles.css'
 import soundtrackBackground from '../../static/Menikahimu_Kahitna.mp3'
 
 export default function SoundTrackPlayer() {
-  return <ReactAudioPlayer src={soundtrackBackground} autoPlay controls />
-  // return (
-  //   <AudioPlayer
-  //     autoPlay
-  //     src={soundtrackBackground}
-  //     loop={true}
-  //     onPlay={(e) => console.log('onPlay')}
-  //   />
-  // )
+  return (
+    <AudioPlayer
+      autoPlay
+      src={soundtrackBackground}
+      loop={true}
+      onPlay={(e) => console.log('onPlay')}
+    />
+  )
 }
